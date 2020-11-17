a79edba60c0bf7adcb91f4b2b27b59af760e043d
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-11-17 15:30:15 +0530

    updated

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>Welcome to Birthday Reminders Portal</title>
 
 	<style type="text/css">
 
@@ -72,20 +72,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 	<div id="body">
 	
-	<a href="birthdays">Birthdays Reminder</a>
-	
+	<a href="birthdays">Birthdays Reminder</a> <br/>
 	<a href="birthdaydetails">Birthday Details</a>
-	
-		<!-- <p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div> -->
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
