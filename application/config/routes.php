68ac1bf1e31088e59f4dd6bb4a410234eb0d2254
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-11-17 14:49:59 +0530

    update

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'welcome';
-$route['birthdays/index'] = 'birthday';
+$route['birthdays/index'] = 'birthdays';        // to call view/birthday_message.php
+$route['birthday/index'] = 'birthday';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
